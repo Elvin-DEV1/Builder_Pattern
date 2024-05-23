b8581f371e9a07cd80eebfe0ef66a112afdb779b
Nguyen Viet Thanh Long <longmatic1.0@gmail.com>
2024-05-23 15:44:44 +0700

    Builder Pattern

---
+++ src/OrderBuilder.java
@@ -0,0 +1,14 @@
+//Builder
+public interface OrderBuilder {
+
+    OrderBuilder orderType(OrderType orderType);
+
+    OrderBuilder orderBread(BreadType breadType);
+
+    OrderBuilder orderSauce(SauceType sauceType);
+
+    OrderBuilder orderVegetable(VegetableType vegetableType);
+
+    Order build();
+
+}
